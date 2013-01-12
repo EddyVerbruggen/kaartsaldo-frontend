1f20e548498b4134a28ee439f1041d0e2af67849
EddyVerbruggen <eddyverbruggen@gmail.com>
2013-01-12 22:22:10 +0100

    initial import

---
+++ js/intratuincard.custom.js
@@ -0,0 +1,9 @@
+"use strict";
+
+function isAndroid() {
+  return navigator.userAgent.toLowerCase().indexOf("android") > -1;
+}
+
+function isIOS() {
+  return navigator.userAgent.match(/(iPad|iPhone|iPod)/i);
+}
---
+++ js/jqm-init.js
@@ -0,0 +1,15 @@
+"use strict";
+
+// override some jqm defaults, need to be done before jqm is loaded
+$(document).bind("mobileinit", function(){
+  $.mobile.page.prototype.options.domCache = true;
+  $.mobile.pushStateEnabled = false;
+  $.mobile.defaultPageTransition = "slide";
+  $.event.special.swipe.horizontalDistanceThreshold = 60; // default is 30, but swipe is triggered too often for my taste
+
+  // page loading widget (only required for login)
+  $.mobile.loader.prototype.options.text = "U wordt ingelogd..";
+  $.mobile.loader.prototype.options.textVisible = true;
+  $.mobile.loader.prototype.options.theme = "b";
+//  $.mobile.loader.prototype.options.html = "";
+});
