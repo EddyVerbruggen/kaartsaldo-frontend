afd90bcfad6444390188d501e4e713305c0d08d1
EddyVerbruggen <eddyverbruggen@gmail.com>
2013-01-13 00:04:40 +0100

    Added images and links of other cardcompanies

--- js/jqm-init.js
+++ js/jqm-init.js
@@ -8,7 +8,7 @@ $(document).bind("mobileinit", function(){
   $.event.special.swipe.horizontalDistanceThreshold = 60; // default is 30, but swipe is triggered too often for my taste
 
   // page loading widget (only required for login)
-  $.mobile.loader.prototype.options.text = "U wordt ingelogd..";
+  $.mobile.loader.prototype.options.text = "Momentje..";
   $.mobile.loader.prototype.options.textVisible = true;
   $.mobile.loader.prototype.options.theme = "b";
 //  $.mobile.loader.prototype.options.html = "";
