ac8665801d4d9d479c4a65189b9397dece68e3e7
EddyVerbruggen <eddyverbruggen@gmail.com>
2013-03-16 17:50:35 +0100

    Maanden aangevuld. Duh.

--- js/kaartsaldo.custom.js
+++ js/kaartsaldo.custom.js
@@ -51,6 +51,15 @@ function getMonthName(nr) {
     case 1 : return "januari";
     case 2 : return "februari";
     case 3 : return "maart";
-    default : return "erm, todo ;)";
+    case 4 : return "april";
+    case 5 : return "mei";
+    case 6 : return "juni";
+    case 7 : return "juli";
+    case 8 : return "augustus";
+    case 9 : return "september";
+    case 10 : return "oktober";
+    case 11 : return "november";
+    case 12 : return "december";
+    default : return "";
   }
 }
